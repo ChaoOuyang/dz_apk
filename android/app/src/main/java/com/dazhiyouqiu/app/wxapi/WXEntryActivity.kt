fdf371522864a6a48169ce982e8651266ee15fa1
ouyangchao <chao_oy@163.com>
2026-02-09 14:53:17 +0800

    初始化wechat-lib的安卓配置

---
+++ android/app/src/main/java/com/dazhiyouqiu/app/wxapi/WXEntryActivity.kt
@@ -0,0 +1,13 @@
+package com.dazhiyouqiu.app.wxapi
+
+import android.app.Activity
+import android.os.Bundle
+import com.wechatlib.WeChatLibModule
+
+class WXEntryActivity : Activity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    WeChatLibModule.handleIntent(intent)
+    finish()
+  }
+}
